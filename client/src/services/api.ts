961798675240d43fc3bfa41f6eba34ea8db266f6
yaswanthreddiee <157574106+yaswanthreddiee@users.noreply.github.com>
2026-07-24 07:24:36 +0530

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.25.13.73:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
